0bd128c28def8a72b13917a1ddaf1b2d2d4eccc4
Rahul Kumar <rahulkumar05au@gmail.com>
2026-08-02 21:44:22 +0530

    Update server.js routing issue solved

--- PacketAnalyzerWeb/server.js
+++ PacketAnalyzerWeb/server.js
@@ -12,7 +12,7 @@ const storage = multer.diskStorage({
         const uploadDir = path.join(__dirname, 'uploads');
         if (!fs.existsSync(uploadDir)) {
             fs.mkdirSync(uploadDir); 
-        }
+        } 
         cb(null, uploadDir);
     },
     filename: (req, file, cb) => {
